712924bea856f859581a69667862d40f355d78c7
Mar-Po <pohancenik.marek@gmail.com>
2021-11-04 19:40:39 +0100

    Update_cv4

--- vrs_cvicenie_4-zadanie_cv4/Src/main.c
+++ vrs_cvicenie_4-zadanie_cv4/Src/main.c
@@ -1,90 +1,74 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "assignment.h"
 
 void SystemClock_Config(void);
-uint8_t check_button_state(GPIO_TypeDef* PORT, uint8_t PIN);
 
 uint8_t switch_state = 0;
 
+
 int main(void)
 {
-  /*Default system setup*/
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+   /*Default system setup*/
+   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
-  SystemClock_Config();
+   NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
+   SystemClock_Config();
 
-  /*
-   * TASK - configure MCU peripherals so that button triggers external interrupt - EXTI.
-   * Button must be connected to the GPIO port B, pin 4.
-   * LED must be connected to the GPIO port A and its pin 4.
-   *
-   * Adjust values of macros defined in "assignment.h".
-   * Implement function "checkButtonState" declared in "assignment.h".
-   */
+   NVIC_SetPriority(EXTI4_IRQn, 2); // priority setup
+   NVIC_EnableIRQ(EXTI4_IRQn); // interrupt setup
 
+   //set EXTI source PB4/
+   SYSCFG->EXTICR[1] &= ~(0xFU);
+   SYSCFG->EXTICR[1] |= (0x1U);
 
-  /* Configure external interrupt - EXTI*/
+   //Enable interrupt from EXTI line 4
+   EXTI->IMR |= EXTI_IMR_MR4;
 
-  	  //type your code for EXTI configuration (priority, enable EXTI, setup EXTI for input pin, trigger edge) here:
+   //Set EXTI trigger to falling edge
+   EXTI->RTSR &= ~(EXTI_IMR_MR4);
+   EXTI->FTSR |= EXTI_IMR_MR4;
 
+   //GPIOB enable
+   RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
 
-  /* Configure GPIOB-4 pin as an input pin - button */
+   //GPIO PB4 input configuration
+   GPIOB->MODER &= ~(GPIO_MODER_MODER4);
+   GPIOB->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR4);
+   GPIOB->OTYPER &= ~(GPIO_OTYPER_OT_4);
+   GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR4);
+   GPIOB->PUPDR |= (GPIO_PUPDR_PUPDR4_0);
 
-	  //type your code for GPIO configuration here:
+   //GPIOA enable
+   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-
-  /* Configure GPIOA-4 pin as an output pin - LED */
-
-	  //type your code for GPIO configuration here:
+   //GPIO PA4 output configuration
+   GPIOA->MODER &= ~(GPIO_MODER_MODER4);
+   GPIOA->MODER |= GPIO_MODER_MODER4_0;
+   GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_4);
+   GPIOA->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR4);
+   GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR4);
 
 
   while (1)
   {
-	  // Modify the code below so it sets/resets used output pin connected to the LED
 	  if(switch_state)
 	  {
-		  GPIOB->BSRR |= GPIO_BSRR_BS_3;
+		  GPIOA->BSRR |= GPIO_BSRR_BS_4;
 		  for(uint16_t i=0; i<0xFF00; i++){}
-		  GPIOB->BRR |= GPIO_BRR_BR_3;
+		  GPIOA->BRR |= GPIO_BRR_BR_4;
 		  for(uint16_t i=0; i<0xFF00; i++){}
 	  }
 	  else
 	  {
-		  GPIOB->BRR |= GPIO_BRR_BR_3;
+		  GPIOA->BRR |= GPIO_BRR_BR_4;
 	  }
   }
 
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
@@ -119,7 +103,32 @@ void SystemClock_Config(void)
 
 uint8_t checkButtonState(GPIO_TypeDef* PORT, uint8_t PIN, uint8_t edge, uint8_t samples_window, uint8_t samples_required)
 {
-	  //type your code for "checkButtonState" implementation here:
+		uint8_t button_state = 0, timeout = 0;
+
+			while(button_state < samples_required && timeout < samples_window)
+			{
+				if(!(PORT->IDR & (edge << PIN))/*LL_GPIO_IsInputPinSet(PORT, PIN)*/)
+				{
+					button_state += 1;
+				}
+				else
+				{
+					button_state = 0;
+				}
+
+				timeout += 1;
+				LL_mDelay(1);
+			}
+
+			if((button_state >= samples_required) && (timeout <= samples_window))
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+
 }
 
 
@@ -135,8 +144,8 @@ void EXTI4_IRQHandler(void)
 	}
 
 	/* Clear EXTI4 pending register flag */
+	EXTI->PR |= (EXTI_PR_PIF4);
 
-		//type your code for pending register flag clear here:
 }
 
 /* USER CODE BEGIN 4 */
